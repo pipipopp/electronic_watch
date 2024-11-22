1afcc1a58bed32a616738c349812a8b0ab5ed1fa
pipipopp <ruikcoldwar12@gmail.com>
2024-11-22 17:28:23 +0200

    utc apdete

--- cspirt.js
+++ cspirt.js
@@ -4,15 +4,21 @@ function updateClock(){
         const hours = String(now.getUTCHours() + time).padStart(2, "0");
         const minutes = String(now.getUTCMinutes()).padStart(2, "0");;
         const seconds = String(now.getUTCSeconds()).padStart(2, "0");
-        document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`
+        document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+
+        const selectTimeZona = document.getElementById('timeZona');
+    timeZona = parseInt(selectTimeZona.value, 10);
+    console.log(timeZona);
 }
 
-//let timeZona = 0;
+let timeZona = 0;
 
 setInterval(updateClock(timeZona), 1000);
-//selectTimeZona.addEventListener('change', updateClock(timeZona));
+selectTimeZona.addEventListener('change', updateClock(timeZona));
 
 // const difrent_time_UTC = new Date().getTimezoneOffset() / 60;
 // const timeZona = Intl.DateTimeFormat().resolvedOptions().timeZone;
 // const timeZona2 = new Date().DateTimeFormat().resolvedOptions().timeZone;
 // document.getElementById('clock2').textContent = timeZona2;
+
+}
