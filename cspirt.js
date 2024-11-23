cff4ff4ef23f2e17715dfe0c985ea8dfae89d569
pipipopp <ruikcoldwar12@gmail.com>
2024-11-23 16:28:41 +0200

    enter utc time with select country

--- cspirt.js
+++ cspirt.js
@@ -1,24 +1,23 @@
-function updateClock(){
-    function updateClock(time) {
-        const now = new Date();
-        const hours = String(now.getUTCHours() + time).padStart(2, "0");
-        const minutes = String(now.getUTCMinutes()).padStart(2, "0");;
-        const seconds = String(now.getUTCSeconds()).padStart(2, "0");
-        document.getElementById('clock').textContent = `${hours}:${minutes}:${seconds}`;
+const clockElement = document.getElementById('clock');
+const selectTimeZona = document.getElementById('timeZona');
+const local_timeZona = document.getElementById("local_timeZona")
 
-        const selectTimeZona = document.getElementById('timeZona');
-    timeZona = parseInt(selectTimeZona.value, 10);
-    console.log(timeZona);
+function updateClock() {
+    const now = new Date();
+    const timeZona = parseInt(selectTimeZona.value, 10);
+    //console.log(timeZona);
+    const hours = String(now.getUTCHours() + timeZona).padStart(2, "0");
+    const minutes = String(now.getUTCMinutes()).padStart(2, "0");;
+    const seconds = String(now.getUTCSeconds()).padStart(2, "0");
+    clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+    local_timeZona.textContent = Intl.DateTimeFormat().resolvedOptions().timeZone;
 }
 
-let timeZona = 0;
+setInterval(updateClock, 1000);
+selectTimeZona.addEventListener('change', updateClock());
 
-setInterval(updateClock(timeZona), 1000);
-selectTimeZona.addEventListener('change', updateClock(timeZona));
+//  const difrent_time_UTC = new Date().getTimezoneOffset() / 60;
+//  const timeZona = Intl.DateTimeFormat().resolvedOptions().timeZone;
+//  const timeZona2 = new Date().DateTimeFormat().resolvedOptions().timeZone;
+//  document.getElementById('clock2').textContent = timeZona2;
 
-// const difrent_time_UTC = new Date().getTimezoneOffset() / 60;
-// const timeZona = Intl.DateTimeFormat().resolvedOptions().timeZone;
-// const timeZona2 = new Date().DateTimeFormat().resolvedOptions().timeZone;
-// document.getElementById('clock2').textContent = timeZona2;
-
-}
